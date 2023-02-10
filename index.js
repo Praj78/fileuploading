7918968ad002974d8228e13e53363f3b56eddfc7
Prajwal <prajwal.yeri@gmail.com>
2023-02-10 22:23:48 +0530

    added database

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app=express()
 
 app.use(express.json())
-app.use(express.static('content'))
+app.use(express.static('Content'))
+const {connectDB}=require('./config/db')
 
-
+app.use(express.urlencoded({extends:false}))
 
 
 const PORT = 1338
 app.listen(PORT,()=>{
     console.log('server is running')
+    connectDB()
 })
