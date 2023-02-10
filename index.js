e504f09fd0567ff5872d3041ad977335c113209f
Prajwal <prajwal.yeri@gmail.com>
2023-02-10 08:51:22 +0530

    updated in index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app=express()
+
+app.use(express.json())
+app.use(express.static('content'))
+
+
+
+
+const PORT = 1338
+app.listen(PORT,()=>{
+    console.log('server is running')
+})
