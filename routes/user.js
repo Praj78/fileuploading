f2ff34901a80503c652c4d06fa1a125f97870be6
Prajwal <prajwal.yeri@gmail.com>
2023-02-14 22:36:10 +0530

    signin and signout added

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express=require('express')
 const router=express.Router()
 const bcrypt=require('bcrypt')
 const User=require('../models/userModel')
+const jwt =require('jsonwebtoken')
 const {
     validateName,
     validateEmail,
@@ -37,10 +38,56 @@ router.post('/signup',async(req,res)=>{
             
         })
     }catch(e){
-        console.log('>>>',e)
         return res.status(500).json({err:e.message})
     }
  
 })
 
+router.post('/signin',async(req,res)=>{
+    try{
+        const {email,password} = req.body
+        if(email.length === 0){
+            return res.status(400).json({
+                err:"please provide email"
+            })
+        }
+        if(password.length === 0){
+            return res.status(400).json({
+                err:"please provide password"
+            })
+        }
+        const existingUser = await User.findOne({where:{email}})
+        if(!existingUser){
+            return res.status(404).json({
+                err:"user not found"
+            })
+        }
+        const passwordMatched = await bcrypt.compare(password,existingUser.password)
+        if(!passwordMatched){
+            return res.status(404).json({
+                err:'email or password mismatched'
+            })
+        }
+        const payload ={user:{id:existingUser.id}}
+        const bearerToken =await jwt.sign(payload,"SECRET MESSAGE",{expiresIn:36000})
+        res.cookie('t',bearerToken,{expire:new Date()+6677})
+        return res.status(200).json({
+            bearerToken
+        })
+
+    }catch(e){
+        return res.status(500).send(e)
+    }
+})
+
+router.get('/signout',async(req,res)=>{
+    try{
+        res.clearCookie('t')
+        return res.status(200).json({
+            message:"cookie deleted"
+        })
+    }catch(e){
+        return res.status(500).send(e)
+    }
+})
 module.exports=router
